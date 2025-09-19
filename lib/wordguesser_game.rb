81e8a942c951a682eadd73009835fc1a18c1f3db
Jalen Stephens <jalen.stephens2025@gmail.com>
2025-09-19 15:59:27 +0000

    Trying to fix git errors

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,10 +3,53 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
 
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = '-' * word.length
   end
+ 
+  def guess(letter)
+    if !letter or !letter.match?(/[A-Za-z]/)
+      raise ArgumentError, "Must be a valid Letter!"
+    end
+
+    if @guesses.include? letter.downcase or @wrong_guesses.include? letter.downcase
+      return false
+    end
+    if @word.include? letter.downcase
+      @guesses << letter.downcase
+
+      @word.chars.each_with_index do |ch , ind|
+        if ch == letter.downcase
+          @word_with_guesses[ind] = ch
+        end
+      end
+
+      return true
+    else
+      @wrong_guesses << letter.downcase
+      return true
+    end
+  end
+
+  def check_win_or_lose
+    if @word == @word_with_guesses
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
+  end
+
+
 
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
