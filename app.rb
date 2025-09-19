04b1487724525749c78dd9cf08c5e078fe567997
Jalen Stephens <jalen.stephens2025@gmail.com>
2025-09-19 17:16:26 +0000

    fixed cheating feature

--- app.rb
+++ app.rb
@@ -75,12 +75,20 @@ class WordGuesserApp < Sinatra::Base
   get '/win' do
     ### YOUR CODE HERE ###
     @game = session[:game]
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
 
   get '/lose' do
     ### YOUR CODE HERE ###
     @game = session[:game]
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
 end
